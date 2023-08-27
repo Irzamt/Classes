845c1af9e08c9410323bd78afd11f5686426f621
Irzam Tahir <irzamtahir4@gmail.com>
2023-08-27 17:13:58 +0500

    Mapping

--- class 9/class_9 .js
+++ class 9/class_9 .js
@@ -1,1 +1,19 @@
 "use strict";
+//MAP => returns new array of same length but the elements could be changed in that new array
+//FILTER => returns same length or less
+//FOR EACH => returns nothing
+let numbers = [2, 3, 4, 5, 6, 7, 8, 45, 20, 1000, 10000000];
+// let updatedArray = numbers.map((number) => { // By mapping method
+//     if (number % 2 === 0)
+//         return "even"; 
+//     else
+//         return "odd";
+// });
+// let updatedArray: string[] = []
+// for (let i = 0; i < numbers.length; i++) { // By for loop
+//     if (numbers[i] % 2 === 0)
+//         updatedArray.push("even");
+//     else
+//         updatedArray.push("odd");
+// }
+// console.log(updatedArray);
