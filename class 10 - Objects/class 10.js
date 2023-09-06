db9b877c83597043d6832e6b6a79205a4aebfcc3
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-07 03:06:55 +0500

    done problem objexts in arrays

--- class 10 - Objects/class 10.js
+++ class 10 - Objects/class 10.js
@@ -1,23 +1,21 @@
 "use strict";
-// let car = {
-//     company: "honda",
-//     color: "white",
-//     year: 2023,
-//     isUsed: false,
-//     run: function(){ 
-//         console.log('car is running');
-//     },
-//     stop: (a:number, b:number) => a + b,
-// };
-let car1 = {
-    company: 'toyota',
-    color: 'white',
-    isUsed: false,
-    year: 2023
-};
-let car2 = {
-    company: 'honda',
-    color: 'black',
-    isUsed: false,
-    year: 2023
-};
+//Objects
+/*************************************************************************************************************** */
+let cars = [
+    {
+        company: 'toyota',
+        color: 'white',
+        isUsed: false,
+        year: 2023
+    },
+    {
+        company: 'honda',
+        color: 'white',
+        isUsed: false,
+        year: 2003
+    }
+];
+for (let i = 0; i < cars.length; i++) {
+    console.log('years: ', cars[i].year);
+    console.log('company: ', cars[i].company);
+}
--- class 10 - Objects/class 10.ts
+++ class 10 - Objects/class 10.ts
@@ -1,3 +1,6 @@
+//Objects
+/*************************************************************************************************************** */
+
 // let car = {
 //     company: "honda",
 //     color: "white",
@@ -37,9 +40,10 @@
 // console.log(car.company); //printing the property key
 // console.log(cars[0]); //Printing the index 0
 // console.log(cars[1].company);
-
+/*************************************************************************************************************** */
 
 // DataTypes in Objects
+/*************************************************************************************************************** */
 
 // let car: {company: string, color:string, isUsed:boolean, year:number} = {
 //     company: 'toyota',
@@ -48,19 +52,58 @@
 //     year: 2023
 // };
 
-type carType = { company: string, color: string, isUsed: boolean, year: number }
+// type carType = { company: string, color: string, isUsed: boolean, year: number } // type keyword is used to store dataTypes in a vaiable of objects
 
-let car1: carType = {
-    company: 'toyota',
-    color: 'white',
-    isUsed: false,
-    year: 2023
-};
+// let car1: carType = {
+//     company: 'toyota',
+//     color: 'white',
+//     isUsed: false,
+//     year: 2023
+// };
 
-let car2: carType = {
-    company: 'honda',
-    color: 'black',
-    isUsed: false,
-    year: 2023
-};
+// let car2: carType = {
+//     company: 'honda',
+//     color: 'black',
+//     isUsed: false,
+//     year: 2023
+// };
 
+// let cars: carType[] = [
+//     {
+//         company: 'toyota',
+//         color: 'white',
+//         isUsed: false,
+//         year: 2023
+//     },
+//     {
+//         company: 'toyota',
+//         color: 'white',
+//         isUsed: false,
+//         year: 2023
+//     }
+// ]
+/*************************************************************************************************************** */
+
+// Write a program that print all the car company and year number from the list, make sure car is an object
+
+type carType = { company: string, color: string, isUsed: boolean, year: number };
+
+let cars: carType[] = [
+    {
+        company: 'toyota',
+        color: 'white',
+        isUsed: false,
+        year: 2023
+    },
+    {
+        company: 'honda',
+        color: 'white',
+        isUsed: false,
+        year: 2003
+    }
+];
+
+for (let i:number = 0; i < cars.length; i++){
+    console.log('years: ' ,cars[i].year);
+    console.log('company: ' ,cars[i].company);
+}
