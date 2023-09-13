665f3c0b4a2619a2559a0f23787d51b416306cc4
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-14 02:48:08 +0500

    done setTimeout

--- class 11 - Asyncronous, OOP and Input/class 11 - Asyncronous.js
+++ class 11 - Asyncronous, OOP and Input/class 11 - Asyncronous.js
@@ -1,1 +1,14 @@
 "use strict";
+/*
+Asyncronous Code | Set Timeout Method | Set Interval Method
+*********************************************************************************************************/
+console.log("before async code");
+// function doSomething (){
+//     console.log('print something');
+// }
+// setTimeout(doSomething, 5000) //The function doSomething() will wait 5s
+// short way to write the above code
+setTimeout(() => {
+    console.log('print do something');
+}, 10000);
+console.log('after async code');
