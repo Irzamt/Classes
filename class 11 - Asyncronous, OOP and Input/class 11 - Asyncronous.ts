2e105a9a39a5625188f42ae99a27bd1840c6eb41
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-15 10:59:21 +0500

    new async method with then and catch

--- class 11 - Asyncronous, OOP and Input/class 11 - Asyncronous.ts
+++ class 11 - Asyncronous, OOP and Input/class 11 - Asyncronous.ts
@@ -29,28 +29,28 @@ Promise Handling | Obect Oriented Class Concept
 Class is an entity which represents some Properties and Methods
 */
 
-type CarType = {price: number, color: string, startCar: () => number};
-class Car { 
-    price: number = 0;
-    color: string = 'not defined'
-    constructor (color: string, price: number){ //constrcutor method will automatically be called
-        this.price = price;
-        this.color = color;
-        console.log('constructor'); 
-    }
-    startCar = () => { //This method will not be called automatically
-        console.log('startCar'); 
-        return 0;
-    }
-}
+// type CarType = {price: number, color: string, startCar: () => number};
+// class Car { 
+//     price: number = 0;
+//     color: string = 'not defined'
+//     constructor (color: string, price: number){ //constrcutor method will automatically be called
+//         this.price = price;
+//         this.color = color;
+//         console.log('constructor'); 
+//     }
+//     startCar = () => { //This method will not be called automatically
+//         console.log('startCar'); 
+//         return 0;
+//     }
+// }
 
-let carA: CarType = new Car('red', 234)
-carA.startCar()
+// let carA: CarType = new Car('red', 234)
+// carA.startCar()
 
-let carB: CarType = new Car('blue', 378)
+// let carB: CarType = new Car('blue', 378)
 
-console.log('carA', carA);
-console.log('carB', carB);
+// console.log('carA', carA);
+// console.log('carB', carB);
 
 
 
@@ -62,3 +62,38 @@ console.log('carB', carB);
 //     price: 234,
 //     color: 'red'
 // }; 
+
+/******************************************************************************************************
+ 
+ ******************************************************************************************************/
+// console.log('before async');
+
+// let setIntervalOutID: number = 0;
+// setIntervalOutID = setTimeout (() => {
+//     console.log('setTimeOut async'); 
+// }, 10000)
+
+// if (false)
+//     clearInterval(setIntervalOutID)
+
+// console.log('after async');
+//---------------------------------------------------------------------------------------------------------
+
+console.log('before async code');
+
+
+function PromiseFunction (resolve:any, reject:any){
+    // async
+    if (false)
+        resolve("successfully async code executed")
+    else
+        reject("something went wrong")
+}
+
+const result = new Promise (PromiseFunction)
+
+result
+.then(sucess => console.log('success', sucess))
+.catch(error => console.log('error', error));
+
+console.log('after async code');
