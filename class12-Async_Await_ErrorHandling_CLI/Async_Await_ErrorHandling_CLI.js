875090a45aea0748f44f65c7cce3b73e6e21489a
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-19 02:54:36 +0500

    20:31 mins of #20: TypeScript Async and Await concept in deep

--- class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.js
+++ class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.js
@@ -1,1 +1,12 @@
-"use strict";
+import inquirer from "inquirer";
+console.log('first');
+async function uploadUsers() {
+    inquirer.prompt([{
+            type: 'number',
+            name: 'num1',
+            message: 'Enter num1'
+        }]);
+    console.log('second');
+}
+uploadUsers();
+console.log('third');
---
+++ class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer"
+
+console.log('first');
+
+async function uploadUsers () {
+    inquirer.prompt({
+        type: 'number',
+        name: 'num1',
+        message: 'Enter num1'
+    })
+    console.log('second');
+}
+
+uploadUsers();
+
+console.log('third');
