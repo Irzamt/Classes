73fd1c1a91ba6c934d2b7671b60e19b62e6f25c6
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-23 05:10:00 +0500

    done Async and Await

--- class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.js
+++ class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.js
@@ -1,12 +1,53 @@
 import inquirer from "inquirer";
-console.log('first');
+console.log('first'); //syncronous
+// let result = inquirer.prompt({
+//     type: 'number',
+//     name: 'num1',
+//     message: 'Enter num1'
+// }) 
+// result.catch ((success) => {
+//     let promise = inquirer.prompt({
+//         type: 'number',
+//         name: 'num2',
+//         message: 'Enter num2'
+//     }) 
+//     promise.then ((success) => {
+//         console.log('success', success);
+//     })
+//     .catch ((err) => {
+//         console.log('err', err);
+//     })
+// })
+// .catch ((err) => {
+//     console.log('err', err);
+// })
 async function uploadUsers() {
-    inquirer.prompt([{
+    let result = await inquirer.prompt({
+        type: 'number',
+        name: 'num1',
+        message: 'Enter num1'
+    });
+    console.log("result", result);
+    if (result.num1 > 50) {
+        let result2 = await inquirer.prompt({
             type: 'number',
-            name: 'num1',
-            message: 'Enter num1'
-        }]);
+            name: 'num2',
+            message: 'Enter num2'
+        });
+        console.log("result2", result2);
+        if (result2.num2 > 20) {
+            let result3 = await inquirer.prompt({
+                type: 'number',
+                name: 'num3',
+                message: 'Enter num3'
+            });
+            console.log("result3", result3);
+        }
+    }
     console.log('second');
 }
-uploadUsers();
-console.log('third');
+uploadUsers(); //Calling the function is asyncronous
+console.log('third'); //syncronous
+/*
+if we dont use await key word then we use .then () and .catch() functions
+*/ 
--- class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.ts
+++ class12-Async_Await_ErrorHandling_CLI/Async_Await_ErrorHandling_CLI.ts
@@ -1,16 +1,65 @@
 import inquirer from "inquirer"
 
-console.log('first');
+console.log('first'); //syncronous
 
-async function uploadUsers () {
-    inquirer.prompt({
+// let result = inquirer.prompt({
+//     type: 'number',
+//     name: 'num1',
+//     message: 'Enter num1'
+// }) 
+
+// result.catch ((success) => {
+//     let promise = inquirer.prompt({
+//         type: 'number',
+//         name: 'num2',
+//         message: 'Enter num2'
+//     }) 
+//     promise.then ((success) => {
+//         console.log('success', success);
+//     })
+//     .catch ((err) => {
+//         console.log('err', err);
+//     })
+// })
+// .catch ((err) => {
+//     console.log('err', err);
+// })
+
+async function uploadUsers() { //function body is syncronous that is achieved by adding await keyword
+    let result = await inquirer.prompt({
         type: 'number',
         name: 'num1',
         message: 'Enter num1'
     })
+    console.log("result", result);
+
+    if (result.num1 > 50) {
+        let result2 = await inquirer.prompt({
+            type: 'number',
+            name: 'num2',
+            message: 'Enter num2'
+        })
+        console.log("result2", result2);
+
+        if (result2.num2 > 20) {
+            let result3 = await inquirer.prompt({
+                type: 'number',
+                name: 'num3',
+                message: 'Enter num3'
+            })
+            console.log("result3", result3);
+
+        }
+    }
+
+
     console.log('second');
 }
 
-uploadUsers();
+uploadUsers(); //Calling the function is asyncronous
 
-console.log('third');
+console.log('third'); //syncronous
+
+/*
+if we dont use await key word then we use .then () and .catch() functions
+*/
